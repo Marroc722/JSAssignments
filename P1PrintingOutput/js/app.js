61ce9ff0910d68fbfd99d422aa4ce4b03d1dfd66
Marroc722 <marroc722@ihmeducation.se>
2024-09-12 10:52:11 +0200

    tried

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -2,3 +2,6 @@ console.log("Hello World");
 console.log("My name is Martinique");
 console.log("Jag bor i Blåsut");
 console.log("Jag är 29 år..");
+
+//This is exercise: P4-Variables
+let numberofbottles = 10
