67a86f1784699f5b5dfc5ae252b2aa20667d4096
Marroc722 <marroc722@ihmeducation.se>
2024-09-12 15:30:01 +0200

    finish part1

---
+++ P5Operators/js/app.js
@@ -0,0 +1,29 @@
+// Uppgift 1
+let speedinput = 72;
+let speedoutput = 0;
+speedoutput = speedinput /3.6;
+console.log(speedoutput);
+
+//Uppgift 2
+let minutesinput = 3;
+let secondsoutput = 0;
+secondsoutput = minutesinput * 60;
+console.log(secondsoutput);
+
+// Uppgift3
+let devisioninput1= 11;
+let devisioninput2 = 4;
+let decisionoutput = 0;
+devisionoutput = devisioninput2 / devisioninput2;
+console.log(devisionoutput);
+
+
+// Uppgift 4
+let remainder1  = 11;
+let remainder2 = 4;
+answer = remainder1 % remainder2;
+console.log(answer);
+
+// Uppgift 5
+let radius = 2;
+
