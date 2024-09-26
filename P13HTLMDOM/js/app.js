0342b75978f45966cbead043cb7ea4d6eedd424d
Marroc722 <marroc722@ihmeducation.se>
2024-09-26 19:15:59 +0200

    testHTLMDOM

---
+++ P13HTLMDOM/js/app.js
@@ -0,0 +1,12 @@
+const text = document.getElementById("text");
+text.innerHTML = "Mitt namn är Martinique";
+
+const text2 = document.getElementById("text2");
+text2.innerHTML = "Tärning";
+text2.style.color = "red";
+
+const text3 = document.getElementById("text3");
+text3.innerHTML = "Lets go";
+text3.style.color = "green";
+
+const button = document.getElementById("button");
